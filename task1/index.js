225689e43837254f73d488c228577bf6818ab4ab
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-03-31 14:34:52 +0300

    task1 - done

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,46 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(stringToCalc) {
+  let arrToCalc = stringToCalc.split(' ');
+  let dotsBeforeSign = arrToCalc[0].length;
+  let dotsAfterSign = arrToCalc[2].length;
+  let sign = arrToCalc[1];
+  let allDots = 0;
+
+  if (dotsBeforeSign >= dotsAfterSign) {
+
+    if (sign === '+') {
+      allDots = dotsBeforeSign + dotsAfterSign;
+      console.log(allDots)
+
+    } else if (sign === '-') {
+      allDots = dotsBeforeSign - dotsAfterSign;
+      console.log(allDots)
+
+    } else if (sign === '//') {
+      allDots = dotsBeforeSign / dotsAfterSign;
+      console.log(allDots)
+
+    } else if (sign === '*') {
+      allDots = dotsBeforeSign * dotsAfterSign;
+      console.log(allDots)
+
+    }
+
+    if (allDots <= 0) {
+      return ''
+    } else if (dotsBeforeSign < dotsAfterSign) {
+      return ''
+    }
+
+    let arrOfDots = [];
+
+    for (num = 1; num <= allDots; num++) {
+      arrOfDots.push('.');
+    }
+
+    let stringOfDots = arrOfDots.join('');
+    console.log(stringOfDots);
+
+    return stringOfDots
+  }
 }
+calculate('..... - ...')
